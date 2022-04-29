9e33b97abedda1ab836c298c6f45b7e528f7c078
VeroniqueDM <veronika.dmiteva@gmail.com>
2022-04-29 22:30:28 +0200

    switched to postgres db

--- ArtHub/art/models.py
+++ ArtHub/art/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
 from django.db import models
 
 # Create your models here.
@@ -128,7 +129,7 @@ class Event(models.Model):
 
     price = models.FloatField(
         validators=(
-
+            MinValueValidator(0),
         )
     )
     user = models.ForeignKey(
