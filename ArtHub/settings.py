ca55aa5e5a083e545729c3b8cdcdc8d62b469774
VeroniqueDM <veronika.dmiteva@gmail.com>
2024-01-11 13:49:36 +0100

    removed media url

--- ArtHub/settings.py
+++ ArtHub/settings.py
@@ -191,7 +191,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 # MEDIA_ROOT = BASE_DIR / 'mediafiles'
 # MEDIA_ROOT =  os.path.join(BASE_DIR, 'mediafiles_build', 'media')
 
